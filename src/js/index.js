cf41cfdb78665e106c09ae0119407a713fd0ec18
Alexpcsilva1985 <157631810+Alexpcsilva1985@users.noreply.github.com>
2024-01-29 22:50:43 +0000

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+/*
+
+
+*/
+
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+elementoPlataformas.classList.toggle("ativo");
+});
+
+
+
+
+
+
+
+
